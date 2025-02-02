bc2ab61a48bae3f25701b0efbc4fe92145152fcf
MaciejCCX <126753610+snowc0ne@users.noreply.github.com>
2025-02-03 10:01:42 +1300

    s

---
+++ PSET1/Conditionals Lecture/compare.py
@@ -0,0 +1,9 @@
+x = int(input("What's X? "))
+y = int(input("What's Y? "))
+
+if x < y:
+    print("X is less than Y")
+if x > y:
+    print("X is greater than Y")
+if x == y:
+    print("X is equal to Y")
